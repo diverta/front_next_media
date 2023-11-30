ed8c728491d78d55eda4ee95ff286aa9c83b9f1b
KuriharaMina <kurihara@diverta.co.jp>
2023-11-30 16:07:50 +0900

    add: モック静的ページ（TOP・CSS・image）

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector('[data-js="header-scroll"]');
+  header.classList.toggle("-scrolled", window.scrollY > 100);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var toggleButton = document.querySelector(".l-header__nav__toggle");
+
+  var header = document.querySelector(".l-header");
+
+  toggleButton.addEventListener("click", function() {
+    header.classList.toggle("is-open");
+  });
+});
