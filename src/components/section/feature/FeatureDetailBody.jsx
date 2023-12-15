8e3d933fec914bf9a4f2a774f3d8de411916fb80
VortexExpansion <gaurav@diverta.co.jp>
2023-12-15 13:26:22 +0900

    upd feature detail body

--- src/components/section/feature/FeatureDetailBody.jsx
+++ src/components/section/feature/FeatureDetailBody.jsx
@@ -1,8 +1,10 @@
+'use client'
+
+import { getDetails } from "@/components/common/fetchData";
 import Image from "next/image";
 
 const FeatureDetailBody = ({ data }) => {
-  console.log("FeatureDetailBody")
-  console.log(data);
+
   return (
     <section className="c-feature">
       <h2 className="c-heading--lv2">{data.subject}</h2>
@@ -10,47 +12,33 @@ const FeatureDetailBody = ({ data }) => {
         {data.ext_2}
       </div>
       <ul className="c-feature__list">
-        <li className="c-feature__item">
-          <h3 className="c-heading--lv4">大阪・難波 人気食べ歩きスポット10選！</h3>
-          <div className="c-feature__contents">
-            <figure className="c-feature__image">
-            <Image
+        {data.ext_3.map(async (item, index) => {
+          const details = await getDetails(item.module_id);
+          return (
+            <li key={index} className="c-feature__item">
+              <h3 className="c-heading--lv4">{details.subject}</h3>
+              <div className="c-feature__contents">
+                <figure className="c-feature__image">
+                <Image
                 alt="dummy image"
-                src={data.ext_1.url}
+                src={details.ext_1.url}
                 width="400"
                 height="150"
               />
-            </figure>
-            <div className="c-feature__detail">
-              <div className="c-feature__text">
-                大阪の人気エリア難波。昔ながらの名店から今年イチオシのおすすめスポットまで。大阪食い倒れツアーをご紹介します。
+                </figure>
+                <div className="c-feature__detail">
+                  <div className="c-feature__text">
+                    {details.ext_2}
+                  </div>
+                  <p className="c-feature__link">
+                    <a href={`../../${details.contents_type_nm.toLowerCase()}/${details.topics_id}`} className="c-button">MORE</a>
+                  </p>
+                </div>
               </div>
-              <p className="c-feature__link"><a href="#" class="c-button">MORE</a></p>
-            </div>
-          </div>
-        </li>
-        <li className="c-feature__item">
-          <h3 className="c-heading--lv4">大阪・難波 人気食べ歩きスポット10選！</h3>
-          <div className="c-feature__contents">
-            <figure className="c-feature__image">
-              {/* <img src="https://placehold.jp/400x300.png"> */}
-              <Image
-                alt="dummy image"
-                src={data.ext_1.url}
-                width="400"
-                height="150"
-              />
-            </figure>
-            <div className="c-feature__detail">
-              <div className="c-feature__text">
-                大阪の人気エリア難波。昔ながらの名店から今年イチオシのおすすめスポットまで。大阪食い倒れツアーをご紹介します。
-              </div>
-              <p className="c-feature__link"><a href="#" class="c-button">MORE</a></p>
-            </div>
-          </div>
-        </li>
+            </li>
+          );
+        })}
       </ul>
-
     </section>
   );
 };
