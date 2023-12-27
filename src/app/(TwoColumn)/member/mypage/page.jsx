fb95a5ab83de8248d08b5787b01530f4a9121137
YuseiAbe <yabe@diverta.co.jp>
2023-12-27 20:25:22 +0900

    feat: apply login feat

--- src/app/(TwoColumn)/member/mypage/page.jsx
+++ src/app/(TwoColumn)/member/mypage/page.jsx
@@ -1,25 +1,74 @@
+'use client';
+
 import { getServerSession } from "next-auth";
 import { getMemberInfo } from "@/components/common/fetchData";
+import { useCallback, useRef } from "react";
 
-export default async function Mypage(){
+export default function Mypage() {
 
-    const session = await getServerSession();
-    console.log("session");
-    console.log(session);
-    const data = await getMemberInfo();
-    console.log(data);
-    if(session){
-        return (
-            <div>
-                {session?.user?.name}さん、こんにちは！ <br />
-                {/* {data} */}
-                <a href="/api/auth/signout">ログアウト</a>
-            </div>
-        )
-    }
+    const email = useRef('');
+    const password = useRef('');
+
+    const login = useCallback(
+        async (event) => {
+            console.log('EVENT', event)
+            event.preventDefault();
+            const credentials = {
+                email: email.current.value,
+                password: password.current.value
+            };
+            console.log(credentials);
+
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/login`,
+                {
+                    method: "POST",
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" },
+                    credentials: 'include'
+                }
+            );
+            const userRef = await fetch(
+                `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/profile`,
+                {
+                    method: "GET",
+                    //   body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" },
+                    credentials: 'include'
+                }
+            );
+            const user = await res.json();
+            const userRef2 = await userRef.json();
+            console.log(user, userRef2);
+
+            // TODO
+            // Now you can fetch both user data then store it into Provider, then you can use it everywhere.
+            // Besides, we should retrieve that user data for initial access with requesting Profile API.
+            // (maybe layout.jsx is the best place to do that.)
+
+            // If no error and we have user data, return it
+            if (res.ok && user) {
+                return user;
+            }
+            // Return null if user data could not be retrieved
+            return null;
+        },
+        []
+    );
+
     return (
-        <div>
-            <a href="/api/auth/signin">ログイン</a>
-        </div>
+        <form className="c-form" onSubmit={login}>
+            <div className="c-form-group">
+                <label htmlFor="email" className="c-form-label">メールアドレス</label>
+                <input name="email" type="email" id="email" ref={email} />
+            </div>
+            <div className="c-form-group">
+                <label htmlFor="password" className="c-form-label">パスワード</label>
+                <input name="password" type="password" ref={password} />
+            </div>
+            <div className="c-form-group">
+                <button type="submit" className="c-button--primary u-width-100">ログイン</button>
+            </div>
+        </form>
     )
 }
