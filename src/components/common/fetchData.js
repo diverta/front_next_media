6354c5e3da7108b848745e4faac57f39dfa9f7f7
VortexExpansion <gaurav@diverta.co.jp>
2024-03-06 12:25:22 +0900

    rmv no-store to test build

--- src/components/common/fetchData.js
+++ src/components/common/fetchData.js
@@ -17,14 +17,14 @@ export async function getContentList(
   if (contentCategory) url += `&contents_type=${categoryID}`;
   if (tag_id) url += `&tag_id[]=${tag_id}`;
   if (search) url += `&filter=keyword%20contains%20${search}`;
-  const res = await fetch(url, { cache: "no-store" });
+  const res = await fetch(url);
   const data = await res.json();
   return data;
 }
 
 export async function getAllContentList() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/content/all`,{ cache: "no-store" }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/content/all`
   );
   const data = await res.json();
   return data.list;
@@ -32,8 +32,7 @@ export async function getAllContentList() {
 
 export async function getCategoryList() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/category/list`,
-    { cache: "no-store" }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/category/list`
   );
   const data = await res.json();
   return data.list;
@@ -45,8 +44,7 @@ export async function getDetails(id) {
     { 
       method: "GET",
       headers: { "Content-Type": "application/json" },
-      credentials: "include",
-      cache: "no-store",
+      credentials: "include"
     }
   );
   const data = await res.json();
@@ -55,8 +53,7 @@ export async function getDetails(id) {
 
 export async function getFeatureList() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/pickup/list`,
-    { cache: "no-store" }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/pickup/list`
   );
   const data = await res.json();
   return data.list;
@@ -64,8 +61,7 @@ export async function getFeatureList() {
 
 export async function getFeatureDetails(id) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/pickup/details/${id}`,
-    { cache: "no-store" }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/pickup/details/${id}`
   );
   const data = await res.json();
   return data.details;
@@ -73,8 +69,7 @@ export async function getFeatureDetails(id) {
 
 export async function getLimitedContent() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/limited-content`,
-    { cache: "no-store" }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/limited-content`
   );
   const data = await res.json();
   return data.list;
@@ -82,8 +77,7 @@ export async function getLimitedContent() {
 
 export async function getTagArea() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/tag/area`,
-    { cache: "no-store" }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/tag/area`
   );
   const data = await res.json();
   return data.list;
@@ -91,8 +85,7 @@ export async function getTagArea() {
 
 export async function getTagKeyword() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/tag/keyword`,
-    { cache: "no-store" }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/tag/keyword`
   );
   const data = await res.json();
   return data.list;
@@ -100,8 +93,7 @@ export async function getTagKeyword() {
 
 export async function getRanking() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/ranking`,
-    { cache: "no-store" } // might need to fetch everytime due to dyanmic update of favorite_cnt
+    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/ranking`
   );
   const data = await res.json();
   return data.list;
@@ -114,8 +106,7 @@ export async function getMyFavoriteList() {
       method: "GET",
       // body: JSON.stringify(params),
       headers: { "Content-Type": "application/json" },
-      credentials: "include",
-      cache: "no-store",
+      credentials: "include"
     }
   );
   const data = await res.json();
@@ -133,8 +124,7 @@ export async function postFavorite(module_id) {
       method: "POST",
       body: JSON.stringify(params),
       headers: { "Content-Type": "application/json" },
-      credentials: "include",
-      cache: "no-store",
+      credentials: "include"
     }
   );
 
