fd7440129d3385f95a097a9a101405beae20116c
YuseiAbe <yabe@diverta.co.jp>
2024-03-23 17:29:12 +0900

    refactor: rm layout.js

--- src/app/feature/[id]/page.jsx
+++ src/app/feature/[id]/page.jsx
@@ -1,3 +1,9 @@
+import Banner from "@/components/common/Banner";
+import Breadcrumb from "@/components/common/Breadcrumb";
+import PageTitle from '@/components/common/PageTitle';
+import TagArea from "@/components/common/TagArea";
+import TagKeyword from "@/components/common/TagKeyword";
+import Feature from "@/components/section/feature/Feature";
 import { getFeatureDetails, getFeatureList, getDetails } from "@/components/common/fetchData";
 import FeatureDetailBody from "@/components/section/feature/FeatureDetailBody";
 
@@ -10,13 +16,13 @@ export async function generateStaticParams() {
 }
 
 export default async function Event({ params }) {
-  const item = await getFeatureDetails(params.id);
+  const features = await getFeatureDetails(params.id);
 
   const fetchDetailsForItems = async () => {
-    if (!item?.relatedContents || item.relatedContents.length === 0) {
+    if (!features?.relatedContents || features.relatedContents.length === 0) {
       return [];
     }
-    const ids = item.relatedContents
+    const ids = features.relatedContents
       .map((item) => item.module_id)
       .filter(id => id)
     return await Promise.all(ids.map((id) => getDetails(id)));
@@ -24,8 +30,25 @@ export default async function Event({ params }) {
   const details = await fetchDetailsForItems();
 
   return (
-    <div>
-      <FeatureDetailBody data={item} details={details} />
+    <div className="l-container">
+      <Breadcrumb paths={[{ label: features?.[0]?.contents_type_ext_col_01 }]} />
+      <PageTitle
+        title={features?.[0]?.contents_type_ext_col_01}
+        subTitle={features?.[0]?.contents_type_nm}
+      />
+      <div className="l-container--col-2 l-container--contents">
+        <div className="l-container--col-2__main">
+          <div>
+            <FeatureDetailBody data={features} details={details} />
+          </div>
+        </div>
+        <div className="l-container--col-2__side">
+          <Banner />
+          <Feature />
+          <TagArea />
+          <TagKeyword />
+        </div>
+      </div>
     </div>
   );
 }
