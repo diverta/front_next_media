53b3aaee66004014b38f0c2cabcc80c47051c7a6
YuseiAbe <yabe@diverta.co.jp>
2024-03-29 11:36:13 +0900

    feat: use next font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,24 @@
 import "@/styles/style.scss";
+
+import { Noto_Sans_JP, Secular_One } from 'next/font/google'
+
+import clsx from "clsx";
 import { UserProvider } from "../contexts/user";
 import Header from "@/components/layouts/Header";
 import Footer from "@/components/layouts/Footer";
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  variable: '--font-noto-sans-jp',
+  display: 'swap',
+});
+
+const secularOne = Secular_One({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-secular-one',
+  display: 'block',
+});
 
 export const metadata = {
   title: "kuroco Trip",
@@ -10,7 +27,7 @@ export const metadata = {
 
 export default async function RootLayout({ children, initialUser }) {
   return (
-    <html>
+    <html className={clsx(notoSansJP.variable, secularOne.variable)}>
       <body>
         <UserProvider>
           <Header />
