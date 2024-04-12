fee04a1de2b5849c1b2a452e82f4b5e5343ba7d7
VortexExpansion <gaurav@diverta.co.jp>
2024-04-12 19:08:13 +0900

    convert feature [id] to CSR

--- src/app/feature/[id]/page.jsx
+++ src/app/feature/[id]/page.jsx
@@ -4,7 +4,6 @@ import PageTitle from '@/components/common/PageTitle';
 import TagArea from '@/components/common/TagArea';
 import TagKeyword from '@/components/common/TagKeyword';
 import Feature from '@/components/section/feature/Feature';
-import getDetails from '@/fetch/getDetails';
 import getFeatureList from '@/fetch/getFeatureList';
 import getFeatureDetails from '@/fetch/getFeatureDetails';
 import FeatureDetailBody from '@/components/section/feature/FeatureDetailBody';
@@ -19,28 +18,19 @@ export async function generateStaticParams() {
 export default async function Page({ params }) {
   const features = await getFeatureDetails(params.id);
 
-  const fetchDetailsForItems = async () => {
-    const contentsPromises = features.relatedContents
-      .map((item) => item.module_id)
-      .filter((id) => id)
-      .map(getDetails);
-    return await Promise.all(contentsPromises);
-  };
-  const details = await fetchDetailsForItems();
-
   return (
     <div className='l-container'>
       <Breadcrumb
-        paths={[{ label: features?.[0]?.contents_type_ext_col_01 }]}
+        paths={[{ label: features?.contents_type_ext_col_01 }]}
       />
       <PageTitle
-        title={features?.[0]?.contents_type_ext_col_01}
-        subTitle={features?.[0]?.contents_type_nm}
+        title={features?.contents_type_ext_col_01}
+        subTitle={features?.contents_type_nm}
       />
       <div className='l-container--col-2 l-container--contents'>
         <div className='l-container--col-2__main'>
           <div>
-            <FeatureDetailBody data={features} details={details} />
+            <FeatureDetailBody params={params} />
           </div>
         </div>
         <div className='l-container--col-2__side'>
