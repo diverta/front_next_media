4727b4375a68563087b0941ef99fae9288a6b91e
VortexExpansion <gaurav@diverta.co.jp>
2024-05-16 20:04:30 +0530

    rmv comment

--- src/app/robots.js
+++ src/app/robots.js
@@ -3,7 +3,6 @@ export default function robots() {
       rules: {
         userAgent: '*',
         allow: '/',
-        // disallow: '/private/',
       },
       sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
     }
