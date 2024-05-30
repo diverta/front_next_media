b3eeaeeb2ca638d525889b8eebd0c3fdc5871338
VortexExpansion <gaurav@diverta.co.jp>
2024-05-30 15:59:06 +0900

    fix format

--- src/fetch/getAllContentList.js
+++ src/fetch/getAllContentList.js
@@ -3,9 +3,10 @@ export default async function getAllContentList(page = 1) {
     `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/1/content/all`,
   );
   url.searchParams.append('pageID', page);
-
-  const { list, pageInfo } = await fetch(url).then(response => response.json());
+  const { list, pageInfo } = await fetch(url).then((response) =>
+    response.json(),
+  );
   const hasMorePage = pageInfo.pageNo < pageInfo.totalPageCnt;
 
-  return [...list,...(hasMorePage ? await getAllContentList(page + 1) : [])];
+  return [...list, ...(hasMorePage ? await getAllContentList(page + 1) : [])];
 }
