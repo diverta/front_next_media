c270680fb3fc4473f1542d0d017d074bf7672d47
VortexExpansion <gaurav@diverta.co.jp>
2024-05-30 13:26:14 +0900

    fix space

--- src/fetch/getAllContentList.js
+++ src/fetch/getAllContentList.js
@@ -6,6 +6,6 @@ export default async function getAllContentList(page = 1) {
 
   const { list, pageInfo } = await fetch(url).then(response => response.json());
   const hasMorePage = pageInfo.pageNo < pageInfo.totalPageCnt;
-  
+
   return [...list,...(hasMorePage ? await getAllContentList(page + 1) : [])];
 }
