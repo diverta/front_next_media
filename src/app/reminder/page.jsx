34520eba67f0db4ad5f1bbfbf7bcb6b82d894296
mimori <mimori@diverta.co.jp>
2024-06-13 23:03:53 +0900

    fix: correct html

--- src/app/reminder/page.jsx
+++ src/app/reminder/page.jsx
@@ -59,7 +59,7 @@ export default function Page() {
       <Breadcrumb paths={[{ label: 'パスワード再発行' }]} />
       <PageTitle title='パスワード再発行' subTitle='Password Reset' />
       <div className='l-container--small l-container--contents'>
-        <div class='c-box'>
+        <div className='c-box'>
           {alert && <AlertSuccess message={alertMessage} />}
           {token ? (
             <form className='c-form' onSubmit={handleReset}>
