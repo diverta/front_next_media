8e8fd1d1823dc6a9e4f05280111abbea23fc0940
Amon Sawamura <amotarao@gmail.com>
2025-05-13 17:56:04 +0900

    sitemap.js, robots.js に export const dynamic 追加

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,9 +1,11 @@
 export default function robots() {
-    return {
-      rules: {
-        userAgent: '*',
-        allow: '/',
-      },
-      sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
-    }
-  }
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  };
+}
+
+export const dynamic = 'force-static';
