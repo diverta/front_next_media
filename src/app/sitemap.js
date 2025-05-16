9635c70426ac7ae6cca40249d4e4bd2977a7b0cd
VortexExpansion <gaurav@diverta.co.jp>
2025-05-16 11:15:20 +0530

    fix sitemap for front url

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -4,25 +4,28 @@ import getFeatureList from '@/fetch/getFeatureList';
 export default async function sitemap() {
   const items = await getAllContentList();
   const articleDetails = items.map((item) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/article/${item.topics_id}/`,
+    url: `${process.env.NEXT_PUBLIC_FRONT_URL}/article/${item.topics_id}/`,
     lastModified: new Date(item.update_ymdhi),
   }));
 
   const features = await getFeatureList();
   const featureDetails = features.map((item) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/feature/${item.topics_id}/`,
+    url: `${process.env.NEXT_PUBLIC_FRONT_URL}/feature/${item.topics_id}/`,
     lastModified: new Date(item.update_ymdhi),
   }));
 
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/privacy/`,
+      url: `${process.env.NEXT_PUBLIC_FRONT_URL}`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact/`,
+      url: `${process.env.NEXT_PUBLIC_FRONT_URL}/privacy/`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/article/`,
+      url: `${process.env.NEXT_PUBLIC_FRONT_URL}/contact/`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_FRONT_URL}/article/`,
     },
     ...articleDetails,
     ...featureDetails,
